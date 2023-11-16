50bcbad9430334468224cb497a024f4e11fad705
yaswanth <yaswanthm@presidio.com>
2023-11-16 21:30:55 +0530

    Add: Introduced langchain agent

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-python3 install -r requirements.txt - to install the required packages
+python3 install -r requirements.txt - to install the required packages <br>
 streamlit run main.py - to start the server(UI) on localhost 
--- langchain_helper.py
+++ langchain_helper.py
@@ -2,6 +2,10 @@ from langchain.llms import OpenAI
 from dotenv import load_dotenv
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+
 
 load_dotenv()
 
@@ -17,5 +21,17 @@ def generate_pet_name(animal_type, animal_color):
     response = name_chain({'animal_type' : animal_type, 'animal_color' : animal_color})
     return response 
 
+def langchain_agent():
+     llm = OpenAI(temperature=0.5)
+     tools = load_tools(["wikipedia", "llm-math"], llm=llm)
+     agent = initialize_agent(
+         tools, llm, agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+     )
+     result = agent.run(
+         "What is the average age of a dog? Multiply the age by 3"
+     )
+     print(result)
+
 if __name__ == "__main__":
-    print(generate_pet_name("cat", "white"))
+     langchain_agent()
+    # print(generate_pet_name("cat", "white"))
